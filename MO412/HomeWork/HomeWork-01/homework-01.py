cdfeb6f265db7bb94b456f3f643df0087deecac9
Paulo Junio Reis Rodrigues <paulinhogod321@gmail.com>
2020-10-02 00:56:00 -0300

    Trabalho feito

--- MO412/HomeWork/HomeWork-01/homework-01.py
+++ MO412/HomeWork/HomeWork-01/homework-01.py
@@ -9,7 +9,7 @@ import random
 
 def PlotNormalScale(graph):
     degree_freq = np.array(nwx.degree_histogram(graph))
-    degree_freq = degree_freq
+    degree_freq = degree_freq / graph.number_of_nodes()
     degrees = range(len(degree_freq))
     plt.title("Degree distribution normal scale")
     plt.plot(degrees[0:], degree_freq[0:], 'ro-')
@@ -22,23 +22,12 @@ def PlotLogLogScale(graph):
     plt.loglog(degree_sequence, "ro-", marker="o")
     plt.title("Degree distribution log log scale")
     plt.ylabel("Degree")
-    plt.ylabel('Frequency')
+    plt.xlabel('Rank')
     plt.show()
 
 def CustomProbability(sumKmj, beginDegree):
     q = 4/3
     e = 0.00001
-    numerator = beginDegree + e
-    denominator = sumKmj
-    return (numerator/denominator) * q
-
-def CustomProbabilityWithNewSum(graph, beginDegree, j):
-    q = 4/3
-    e = 0.00001
-    sumKmj = 0
-    for i in range(1, j):
-        sumKmj += graph.degree[i] + e
-
     numerator = beginDegree + e
     denominator = sumKmj
     return (numerator/denominator) * q
@@ -61,45 +50,27 @@ def FirstExercise(numberNodes, probability):
 #Every every j interation, do the sum += j.degree + e
 def SecondExercise(numberNodes):
     graph = nwx.Graph()
-    e = 0.00001
-
-    for i in range (0, numberNodes):
-        graph.add_node(i)
 
     for i in range(0, numberNodes):
+        graph.add_node(i)
+
+    for j in range(0, numberNodes):
         sumKmj = 0
-        for j in range(i+1, numberNodes):
+        for m in range(1, j):
+            sumKmj += graph.degree[m] + 0.00001
+        for i in range(1, j):
             if (j > 1):
-                sumKmj += graph.degree[j] + e
                 if (random.random() < CustomProbability(sumKmj, graph.degree[i])):
                     graph.add_edge(i, j)
 
     numberLinks = graph.number_of_edges()
-    print(f'SecondExercise - Generate a graph with {numberNodes} nodes, {numberLinks} links and average degree is {numberLinks*2/numberNodes}')
+    print(f'SecondExerciseWithNewSum - Generate a graph with {numberNodes} nodes, {numberLinks} links and average degree is {numberLinks * 2 / numberNodes}')
     PlotNormalScale(graph)
     PlotLogLogScale(graph)
 
-# Every every j interation, do the sum ( 1 to j )
-def SecondExerciseWithNewSum(numberNodes):
-    graph = nwx.Graph()
-
-    for i in range (0, numberNodes):
-        graph.add_node(i)
-
-    for i in range(0, numberNodes):
-        for j in range(i+1, numberNodes):
-            if (j > 1):
-                if (random.random() < CustomProbabilityWithNewSum(graph, graph.degree[i], j)):
-                    graph.add_edge(i, j)
-
-    numberLinks = graph.number_of_edges()
-    print(f'SecondExerciseWithNewSum - Generate a graph with {numberNodes} nodes, {numberLinks} links and average degree is {numberLinks*2/numberNodes}')
-    PlotNormalScale(graph)
-    PlotLogLogScale(graph)
 
 if __name__ == "__main__":
-    numberNodes = 300
+    numberNodes = 10000
     probability = 0.0004
     FirstExercise(numberNodes, probability)
     SecondExercise(numberNodes)
-    SecondExerciseWithNewSum(numberNodes)
