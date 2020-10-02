a5111eee92c1613cd69dae3ddc869b58678c6aa6
Paulo Junio Reis Rodrigues <paulinhogod321@gmail.com>
2020-10-02 13:30:53 -0300

    Trabalho feito e entregue

--- MO412/HomeWork/HomeWork-01/homework-01.py
+++ MO412/HomeWork/HomeWork-01/homework-01.py
@@ -2,11 +2,14 @@
 First HomeWork
 Student: Paulo Junio Reis Rodrigues
 """
+from collections import Counter
+from operator import itemgetter
 import networkx as nwx
 import matplotlib.pyplot as plt
 import numpy as np
 import random
 
+#Plot degree distribution with normal scale
 def PlotNormalScale(graph):
     degree_freq = np.array(nwx.degree_histogram(graph))
     degree_freq = degree_freq / graph.number_of_nodes()
@@ -17,14 +20,18 @@ def PlotNormalScale(graph):
     plt.ylabel('Frequency')
     plt.show()
 
+#Plot degree distribution with log log scale
 def PlotLogLogScale(graph):
-    degree_sequence = sorted([d for n, d in graph.degree()], reverse=True)
-    plt.loglog(degree_sequence, "ro-", marker="o")
+    degree_freq = np.array(nwx.degree_histogram(graph))
+    degree_freq = degree_freq / graph.number_of_nodes()
+    degrees = range(len(degree_freq))
     plt.title("Degree distribution log log scale")
-    plt.ylabel("Degree")
-    plt.xlabel('Rank')
+    plt.loglog(degrees[0:], degree_freq[0:], 'go-')
+    plt.xlabel('Degree')
+    plt.ylabel('Frequency')
     plt.show()
 
+#Calculate custom probability
 def CustomProbability(sumKmj, beginDegree):
     q = 4/3
     e = 0.00001
@@ -32,6 +39,7 @@ def CustomProbability(sumKmj, beginDegree):
     denominator = sumKmj
     return (numerator/denominator) * q
 
+#Create the network with averege degree = 4
 def FirstExercise(numberNodes, probability):
     graph = nwx.Graph()
     for i in range (0, numberNodes):
@@ -47,30 +55,31 @@ def FirstExercise(numberNodes, probability):
     PlotNormalScale(graph)
     PlotLogLogScale(graph)
 
-#Every every j interation, do the sum += j.degree + e
+#Create the network with averege degree = 2.66
 def SecondExercise(numberNodes):
     graph = nwx.Graph()
 
-    for i in range(0, numberNodes):
+    for i in range(0, numberNodes): #Create graph
         graph.add_node(i)
 
     for j in range(0, numberNodes):
         sumKmj = 0
-        for m in range(1, j):
+        for m in range(1, j): #Do the summation
             sumKmj += graph.degree[m] + 0.00001
         for i in range(1, j):
             if (j > 1):
-                if (random.random() < CustomProbability(sumKmj, graph.degree[i])):
+                if (random.random() < CustomProbability(sumKmj, graph.degree[i])): #Calculate the custom probability
                     graph.add_edge(i, j)
 
     numberLinks = graph.number_of_edges()
-    print(f'SecondExerciseWithNewSum - Generate a graph with {numberNodes} nodes, {numberLinks} links and average degree is {numberLinks * 2 / numberNodes}')
+    print(f'SecondExercise - Generate a graph with {numberNodes} nodes, {numberLinks} links and average degree is {numberLinks * 2 / numberNodes}')
     PlotNormalScale(graph)
     PlotLogLogScale(graph)
 
 
 if __name__ == "__main__":
     numberNodes = 10000
-    probability = 0.0004
+    averegeDegree = 4
+    probability = averegeDegree/(numberNodes - 1)# Ad = P * ( N - 1 ) => Ad/(N - 1) = P
     FirstExercise(numberNodes, probability)
     SecondExercise(numberNodes)
