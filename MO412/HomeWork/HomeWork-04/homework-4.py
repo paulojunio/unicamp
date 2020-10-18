f8ae04f7175162d8d6f6fdea32c307209445be8a
Paulo Junio Reis Rodrigues <paulinhogod321@gmail.com>
2020-10-18 04:20:09 -0300

    Homework-4 quase pronto

--- MO412/HomeWork/HomeWork-04/homework-4.py
+++ MO412/HomeWork/HomeWork-04/homework-4.py
@@ -9,9 +9,7 @@ import random
 
 
 #Plot degree distribution with log log scale
-def PlotLogLogScale(graph):
-    degree_freq = np.array(nwx.degree_histogram(graph))
-    degree_freq = degree_freq / graph.number_of_nodes()
+def PlotLogLogScale(degree_freq):
     degrees = range(len(degree_freq))
     plt.title("Degree distribution log log scale")
     plt.loglog(degrees[0:], degree_freq[0:], 'go-')
@@ -26,11 +24,33 @@ def CreateGraph():
 
     for linha in arquivo_ligacoes:
         valores = linha.split()
-        print(valores[0], valores[1])
+        graph.add_node(valores[0])
+        graph.add_node(valores[1])
+        graph.add_edge(valores[0], valores[1])
 
-    ref_arquivo.close()
-
+    arquivo_ligacoes.close()
     return graph
+
+def OutDegreePlot(graph):
+    graphOutDegree = graph.out_degree
+    allDegrees = np.asarray([x[1] for x in graphOutDegree])
+    degreesOutFreq = np.zeros(allDegrees.max() + 1, dtype=int)
+    for x in allDegrees:
+        degreesOutFreq[x] += 1
+    print(degreesOutFreq / allDegrees.size)
+    PlotLogLogScale(degreesOutFreq / allDegrees.size)
+
+def InDegreePlot(graph):
+    graphInDegree = graph.in_degree
+    allDegrees = np.asarray([x[1] for x in graphInDegree])
+    degreesInFreq = np.zeros(allDegrees.max() + 1, dtype=int)
+    for x in allDegrees:
+        degreesInFreq[x] += 1
+    print(degreesInFreq / allDegrees.size)
+    PlotLogLogScale(degreesInFreq / allDegrees.size)
+
 
 if __name__ == "__main__":
     graph = CreateGraph()
+    OutDegreePlot(graph)
+    InDegreePlot(graph)
