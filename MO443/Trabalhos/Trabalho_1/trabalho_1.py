2b83c55f23843bb1576a323870a6c31fc27b1296
Paulo Junio Reis Rodrigues <paulinhogod321@gmail.com>
2020-10-20 19:27:20 -0300

    Gravando dados novamente

--- MO443/Trabalhos/Trabalho_1/trabalho_1.py
+++ MO443/Trabalhos/Trabalho_1/trabalho_1.py
@@ -26,12 +26,12 @@ HTRES_ARRAY = np.array([[-1, 0, 1],
                         [-1, 0, 1]])
 
 HQUATRO_ARRAY = np.array([[-1, -2, -1],
-                        [0, 0, 0],
-                        [1, 2, 1]])
+                          [0, 0, 0],
+                          [1, 2, 1]])
 
 HCINCO_ARRAY = np.array([[-1, -1, -1],
-                        [-1, 8, -1],
-                        [-1, -1, -1]])
+                         [-1, 8, -1],
+                         [-1, -1, -1]])
 
 HSEIS_ARRAY = np.array([[1/9, 1/9, 1/9],
                         [1/9, 1/9, 1/9],
@@ -64,32 +64,6 @@ HDEZ_ARRAY = np.array([[-1/8, -1/8, -1/8, -1/8, -1/8],
 HONZE_ARRAY = np.array([[-1, -1, 0],
                         [-1, 0, 1],
                         [0, 1, 1]])
-
-# def convolve(image, kernel):
-#     (M, N) = image.shape[:2]
-#     (m, n) = kernel.shape[:2]
-#     m = int(m // 2)
-#     n = int(n // 2)
-#     print(m,n)
-#
-#     for i in np.arange(-1 * m, m + 1):
-#         print()
-#         for j in np.arange(-1 * n, n + 1):
-#             print(kernel[int(i),int(j)], end=" ")
-#
-#     pad = (n - 1) // 2
-#     image = cv2.copyMakeBorder(image, pad, pad, pad, pad, cv2.BORDER_REPLICATE)
-#     output = np.zeros((M, N), dtype="float32")
-#     for y in np.arange(pad, M - 1):
-#         for x in np.arange(pad, N - 1):
-#             soma = 0
-#             for i in np.arange(-1 * m, m + 1):
-#                 for j in np.arange(-1 * n, n + 1):
-#                     soma += kernel[int(i),int(j)] * image[int(x-i),int(y-j)]
-#             output[x,y] = soma
-#     output = rescale_intensity(output, in_range=(0, 255))
-#     output = (output * 255).astype("uint8")
-#     return output
 
 #Normalizacao de uma imagem de acordo com o g_min e o g_max
 def TransformarImagem (imagem, g_min, g_max):
@@ -135,85 +109,105 @@ def convolucao(imagem, filtro):
 def ArquivoDeSaida(filtro):
     return f'outputs/trabalho_1_{filtro}.png'
 
+def AplicarFiltro(imagem, filtro):
+    return cv2.filter2D(imagem, -1, filtro[::-1, ::-1], cv2.BORDER_REPLICATE)
+
+def SomarDuasFiltragens(primeiraImagem, segundaImagem):
+    return np.round(np.sqrt((np.power(primeiraImagem.astype(np.uint16), 2) + np.power(segundaImagem.astype(np.uint16), 2)))).astype(np.uint8)
+
 if __name__ == "__main__":
 
     nomeDoArquivo = sys.argv[1]
     imagemOriginal = cv2.imread(nomeDoArquivo, cv2.IMREAD_GRAYSCALE) #Leitura da imagem original
 
-    output = cv2.filter2D(imagemOriginal, -1, HUM_ARRAY[::-1,::-1])
+    #Aplicando o filtro h1
+    output = AplicarFiltro(imagemOriginal,HUM_ARRAY)
     cv2.imwrite(ArquivoDeSaida('H1'), output)
 
-    output = cv2.filter2D(imagemOriginal, -1, HDOIS_ARRAY[::-1,::-1])
+    # Aplicando o filtro h2
+    output = AplicarFiltro(imagemOriginal,HDOIS_ARRAY)
     cv2.imwrite(ArquivoDeSaida('H2'), output)
 
-    output_coluna = cv2.filter2D(imagemOriginal, -1, HTRES_ARRAY[::-1,::-1])
+    # Aplicando o filtro h3
+    output_coluna = AplicarFiltro(imagemOriginal,HTRES_ARRAY)
     cv2.imwrite(ArquivoDeSaida('H3'), output_coluna)
 
-    output_linha = cv2.filter2D(imagemOriginal, -1, HQUATRO_ARRAY[::-1,::-1])
+    # Aplicando o filtro h4
+    output_linha = AplicarFiltro(imagemOriginal, HQUATRO_ARRAY)
     cv2.imwrite(ArquivoDeSaida('H4'), output_linha)
 
-    output = np.round(np.sqrt((np.power(output_linha.astype(np.uint16), 2) + np.power(output_coluna.astype(np.uint16), 2)))).astype(np.uint8)
+    # Juntando dois filtros, o h3 e o h4
+    output = SomarDuasFiltragens(output_coluna, output_linha) #Fazendo o calculo para juntar os filtros, fornecido pelo professor
     cv2.imwrite(ArquivoDeSaida('H3_H4'), output)
 
-    output = cv2.filter2D(imagemOriginal, -1, HCINCO_ARRAY[::-1,::-1])
+    # Aplicando o filtro h5
+    output = AplicarFiltro(imagemOriginal,HCINCO_ARRAY)
     cv2.imwrite(ArquivoDeSaida('H5'), output)
 
-    output = cv2.filter2D(imagemOriginal, -1, HSEIS_ARRAY[::-1,::-1])
+    # Aplicando o filtro h6
+    output = AplicarFiltro(imagemOriginal, HSEIS_ARRAY)
     cv2.imwrite(ArquivoDeSaida('H6'), output)
 
-    output = cv2.filter2D(imagemOriginal, -1, HSETE_ARRAY[::-1,::-1])
+    # Aplicando o filtro h7
+    output = AplicarFiltro(imagemOriginal,HSETE_ARRAY)
     cv2.imwrite(ArquivoDeSaida('H7'), output)
 
-    output = cv2.filter2D(imagemOriginal, -1, HOITO_ARRAY[::-1,::-1])
+    # Aplicando o filtro h8
+    output = AplicarFiltro(imagemOriginal, HOITO_ARRAY)
     cv2.imwrite(ArquivoDeSaida('H8'), output)
 
-    output = cv2.filter2D(imagemOriginal, -1, HNOVE_ARRAY[::-1,::-1])
-    print(output)
+    # Aplicando o filtro h9
+    output = AplicarFiltro(imagemOriginal, HNOVE_ARRAY)
     cv2.imwrite(ArquivoDeSaida('H9'), output)
 
-    output = cv2.filter2D(imagemOriginal, -1, HDEZ_ARRAY[::-1,::-1])
+    # Aplicando o filtro h10
+    output = AplicarFiltro(imagemOriginal, HDEZ_ARRAY)
     cv2.imwrite(ArquivoDeSaida('H10'), output)
 
-    output = cv2.filter2D(imagemOriginal, -1, HONZE_ARRAY[::-1,::-1])
+    # Aplicando o filtro h11
+    output = AplicarFiltro(imagemOriginal, HONZE_ARRAY)
     cv2.imwrite(ArquivoDeSaida('H11'), output)
 
-    output = convolucao(imagemOriginal, HUM_ARRAY)
-    cv2.imwrite(ArquivoDeSaida('MetodoH1'), output)
 
-    output = convolucao(imagemOriginal, HDOIS_ARRAY)
-    cv2.imwrite(ArquivoDeSaida('MetodoH2'), output)
 
-    output_coluna = convolucao(imagemOriginal, HTRES_ARRAY)
-    cv2.imwrite(ArquivoDeSaida('MetodoH3'), output_coluna)
 
-    output_linha = convolucao(imagemOriginal, HQUATRO_ARRAY)
-    cv2.imwrite(ArquivoDeSaida('MetodoH4'), output_linha)
-
-    output = np.round(
-        np.sqrt((np.power(output_linha.astype(np.uint16), 2) + np.power(output_coluna.astype(np.uint16), 2)))).astype(
-        np.uint8)
-    cv2.imwrite(ArquivoDeSaida('MetodoH3_H4'), output)
-
-    output = convolucao(imagemOriginal, HCINCO_ARRAY)
-    cv2.imwrite(ArquivoDeSaida('MetodoH5'), output)
-
-    output = convolucao(imagemOriginal, HSEIS_ARRAY)
-    cv2.imwrite(ArquivoDeSaida('MetodoH6'), output)
-
-    output = convolucao(imagemOriginal, HSETE_ARRAY)
-    cv2.imwrite(ArquivoDeSaida('MetodoH7'), output)
-
-    output = convolucao(imagemOriginal, HOITO_ARRAY)
-    cv2.imwrite(ArquivoDeSaida('MetodoH8'), output)
-
-    output = convolucao(imagemOriginal, HNOVE_ARRAY)
-    print(output)
-    cv2.imwrite(ArquivoDeSaida('MetodoH9'), output)
-
-    output = convolucao(imagemOriginal, HDEZ_ARRAY)
-    cv2.imwrite(ArquivoDeSaida('MetodoH10'), output)
-
-    output = convolucao(imagemOriginal, HONZE_ARRAY)
-    cv2.imwrite(ArquivoDeSaida('MetodoH11'), output)
+    #==========Outro codigo======Naoentregar
+    # output = convolucao(imagemOriginal, HUM_ARRAY)
+    # cv2.imwrite(ArquivoDeSaida('MetodoH1'), output)
+    #
+    # output = convolucao(imagemOriginal, HDOIS_ARRAY)
+    # cv2.imwrite(ArquivoDeSaida('MetodoH2'), output)
+    #
+    # output_coluna = convolucao(imagemOriginal, HTRES_ARRAY)
+    # cv2.imwrite(ArquivoDeSaida('MetodoH3'), output_coluna)
+    #
+    # output_linha = convolucao(imagemOriginal, HQUATRO_ARRAY)
+    # cv2.imwrite(ArquivoDeSaida('MetodoH4'), output_linha)
+    #
+    # output = np.round(
+    #     np.sqrt((np.power(output_linha.astype(np.uint16), 2) + np.power(output_coluna.astype(np.uint16), 2)))).astype(
+    #     np.uint8)
+    # cv2.imwrite(ArquivoDeSaida('MetodoH3_H4'), output)
+    #
+    # output = convolucao(imagemOriginal, HCINCO_ARRAY)
+    # cv2.imwrite(ArquivoDeSaida('MetodoH5'), output)
+    #
+    # output = convolucao(imagemOriginal, HSEIS_ARRAY)
+    # cv2.imwrite(ArquivoDeSaida('MetodoH6'), output)
+    #
+    # output = convolucao(imagemOriginal, HSETE_ARRAY)
+    # cv2.imwrite(ArquivoDeSaida('MetodoH7'), output)
+    #
+    # output = convolucao(imagemOriginal, HOITO_ARRAY)
+    # cv2.imwrite(ArquivoDeSaida('MetodoH8'), output)
+    #
+    # output = convolucao(imagemOriginal, HNOVE_ARRAY)
+    # cv2.imwrite(ArquivoDeSaida('MetodoH9'), output)
+    #
+    # output = convolucao(imagemOriginal, HDEZ_ARRAY)
+    # cv2.imwrite(ArquivoDeSaida('MetodoH10'), output)
+    #
+    # output = convolucao(imagemOriginal, HONZE_ARRAY)
+    # cv2.imwrite(ArquivoDeSaida('MetodoH11'), output)
 
 
